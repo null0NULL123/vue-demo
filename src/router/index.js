ac8bbc421f48187b26ee9da8bdb862d3fa7bcc2a
kylin <2022270162@email.szu.edu.cn>
2024-12-09 18:53:32 +0800

    remove

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
-import HomeView from "@/views/HomeView.vue";
-import ProfileView from "@/views/ProfileView.vue";
-import MessagesView from "@/views/MessagesView.vue";
-import SettingsView from "@/views/SettingsView.vue";
-import ContentView from "@/views/ContentView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,31 +12,6 @@ const router = createRouter({
     {
       path: "/",
       redirect: "/login",
-    },
-    {
-      path: "/home",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      component: ProfileView,
-    },
-    {
-        path: "/content",
-        name: "content",
-        component: ContentView,
-      },
-    {
-      path: "/messages",
-      name: "messages",
-      component: MessagesView,
-    },
-    {
-        path: "/settings",
-        name: "settings",
-        component: SettingsView,
     },
   ],
 });
